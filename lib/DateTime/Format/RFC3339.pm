d8fe09296a10f213e90157e7f24b5a0f4d55a637
Eric Brine <ikegami@adaelis.com>
2024-01-21 17:59:18 -0400

    v1.8.0

--- lib/DateTime/Format/RFC3339.pm
+++ lib/DateTime/Format/RFC3339.pm
@@ -4,7 +4,7 @@ package DateTime::Format::RFC3339;
 use strict;
 use warnings;
 
-use version; our $VERSION = qv( 'v1.7.0' );
+use version; our $VERSION = qv( 'v1.8.0' );
 
 use Carp     qw( croak );
 use DateTime qw( );
@@ -156,7 +156,7 @@ DateTime::Format::RFC3339 - Parse and format RFC3339 datetime strings
 
 =head1 VERSION
 
-Version 1.7.0
+Version 1.8.0
 
 
 =head1 SYNOPSIS
